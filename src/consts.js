062e022a067ecbe628721b8ecac7c9e76e51fe97
arthuracrs <arthuracrs3@gmail.com>
2023-01-08 18:33:15 -0300

    update

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-    BACKEND_URL: 'http://meuloru:8000',
-    FRONTEND_URL: 'http://meuloru:3001'
+    BACKEND_URL: 'http://meuloru.com:8000',
+    FRONTEND_URL: 'http://meuloru.com:3001'
     // BACKEND_URL: 'http://localhost:3000'
 }
